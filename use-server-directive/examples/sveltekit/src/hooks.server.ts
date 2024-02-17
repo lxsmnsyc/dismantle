68c016f81c255b38e00a470da85fa17fa856310d
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-02-18 00:36:50 +0800

    Update hooks.server.ts

--- use-server-directive/examples/sveltekit/src/hooks.server.ts
+++ use-server-directive/examples/sveltekit/src/hooks.server.ts
@@ -1,7 +1,5 @@
 import { handleRequest } from 'use-server-directive/server';
 
-import 'use-server-directive/preload';
-
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   return (await handleRequest(event.request)) || (await resolve(event));
