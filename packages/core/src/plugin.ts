db0200c9d6a36eb51aec0f9a830f1c9930d3e37e
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-10-26 01:53:36 +0800

    Update plugin.ts

--- packages/core/src/plugin.ts
+++ packages/core/src/plugin.ts
@@ -1,4 +1,5 @@
 import type * as babel from '@babel/core';
+import type * as t from '@babel/types';
 import { transformBlockDirective } from './transform-block-directive';
 import { transformCall } from './transform-call';
 import { transformFunctionDirective } from './transform-function-directive';
@@ -10,30 +11,37 @@ interface State extends babel.PluginPass {
   opts: StateContext;
 }
 
-export function plugin(): babel.PluginObj<State> {
-  return {
-    name: 'dismantle',
-    visitor: {
-      Program(programPath, ctx) {
-        registerImportSpecifiers(ctx.opts, programPath);
-        programPath.traverse({
-          FunctionDeclaration(path) {
-            bubbleFunctionDeclaration(ctx.opts, programPath, path);
-          },
-        });
-      },
-      ArrowFunctionExpression(path, ctx) {
-        transformFunctionDirective(ctx.opts, path);
-      },
-      FunctionExpression(path, ctx) {
-        transformFunctionDirective(ctx.opts, path);
-      },
-      BlockStatement(path, ctx) {
-        transformBlockDirective(ctx.opts, path);
-      },
-      CallExpression(path, ctx) {
-        transformCall(ctx.opts, path);
-      },
+const FUNCTION_BUBBLE: babel.Visitor<{
+  ctx: State;
+  program: babel.NodePath<t.Program>;
+}> = {
+  FunctionDeclaration(path, { ctx, program }) {
+    bubbleFunctionDeclaration(ctx.opts, program, path);
+  },
+};
+
+const PLUGIN: babel.PluginObj<State> = {
+  name: 'dismantle',
+  visitor: {
+    Program(program, ctx) {
+      registerImportSpecifiers(ctx.opts, program);
+      program.traverse(FUNCTION_BUBBLE, { ctx, program });
     },
-  };
+    ArrowFunctionExpression(path, ctx) {
+      transformFunctionDirective(ctx.opts, path);
+    },
+    FunctionExpression(path, ctx) {
+      transformFunctionDirective(ctx.opts, path);
+    },
+    BlockStatement(path, ctx) {
+      transformBlockDirective(ctx.opts, path);
+    },
+    CallExpression(path, ctx) {
+      transformCall(ctx.opts, path);
+    },
+  },
+};
+
+export function plugin(): babel.PluginObj<State> {
+  return PLUGIN;
 }
