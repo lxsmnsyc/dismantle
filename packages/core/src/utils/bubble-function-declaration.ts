d7915b4c371ceec8fd0212b24d3f390cc100d107
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-02-23 13:15:15 +0800

    Update bubble-function-declaration.ts

--- packages/core/src/utils/bubble-function-declaration.ts
+++ packages/core/src/utils/bubble-function-declaration.ts
@@ -2,53 +2,52 @@ import type * as babel from '@babel/core';
 import * as t from '@babel/types';
 import type { StateContext } from '../types';
 import { getDefinitionFromDirectives } from './directive-check';
-import { isStatementTopLevel } from './is-statement-top-level';
 
 export function bubbleFunctionDeclaration(
   ctx: StateContext,
-  program: babel.NodePath<t.Program>,
   path: babel.NodePath<t.FunctionDeclaration>,
 ): void {
-  if (isStatementTopLevel(path)) {
-    const decl = path.node;
-    // Check if declaration is FunctionDeclaration
-    if (decl.id) {
-      const definition = getDefinitionFromDirectives(
-        ctx,
-        'function-directive',
-        path.get('body'),
-      );
-      if (!definition) {
-        return;
-      }
-      const [tmp] = program.unshiftContainer(
-        'body',
-        t.variableDeclaration('const', [
-          t.variableDeclarator(
+  const decl = path.node;
+  // Check if declaration is FunctionDeclaration
+  if (decl.id) {
+    const definition = getDefinitionFromDirectives(
+      ctx,
+      'function-directive',
+      path.get('body'),
+    );
+    if (!definition) {
+      return;
+    }
+    const block = (path.findParent(current => current.isBlockStatement()) ||
+      path.scope.getProgramParent().path) as babel.NodePath<t.BlockStatement>;
+
+    const [tmp] = block.unshiftContainer(
+      'body',
+      t.variableDeclaration('const', [
+        t.variableDeclarator(
+          decl.id,
+          t.functionExpression(
             decl.id,
-            t.functionExpression(
-              decl.id,
-              decl.params,
-              decl.body,
-              decl.generator,
-              decl.async,
-            ),
+            decl.params,
+            decl.body,
+            decl.generator,
+            decl.async,
           ),
+        ),
+      ]),
+    );
+    path.scope.registerDeclaration(tmp);
+    tmp.skip();
+    if (path.parentPath.isExportNamedDeclaration()) {
+      path.parentPath.replaceWith(
+        t.exportNamedDeclaration(undefined, [
+          t.exportSpecifier(decl.id, decl.id),
         ]),
       );
-      program.scope.registerDeclaration(tmp);
-      tmp.skip();
-      if (path.parentPath.isExportNamedDeclaration()) {
-        path.parentPath.replaceWith(
-          t.exportNamedDeclaration(undefined, [
-            t.exportSpecifier(decl.id, decl.id),
-          ]),
-        );
-      } else if (path.parentPath.isExportDefaultDeclaration()) {
-        path.replaceWith(decl.id);
-      } else {
-        path.remove();
-      }
+    } else if (path.parentPath.isExportDefaultDeclaration()) {
+      path.replaceWith(decl.id);
+    } else {
+      path.remove();
     }
   }
 }
